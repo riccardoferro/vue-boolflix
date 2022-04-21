301b6c07d74b30927048c5bf2748caf9c22093ff
riccardoferro <riccardo.ferron8@gmail.com>
2022-04-21 15:56:50 +0200

    imported component film list and sent him the data movies

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>
-
-  </div>
+    <div class="container">
+        <h2>FILM</h2>
+        <FilmList :movies="movies"/>
+    </div>
 </template>
 
 <script>
+
+import FilmList from "@/subcomponents/FilmList.vue"
+
 export default {
-  name: "MainComponent"
+  name: "MainComponent",
+  props: {
+    movies: Array
+  },
+  components: {
+    FilmList
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+
 
 </style>

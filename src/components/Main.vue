f666b45100e41ee0bc2f3d27b84f24ae52c67e25
riccardoferro <riccardo.ferron8@gmail.com>
2022-04-21 16:32:19 +0200

    add series

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,20 +2,25 @@
     <div class="container">
         <h2>FILM</h2>
         <FilmList :movies="movies"/>
+        <h2>SERIE</h2>
+        <SerieList :series="series"/>
     </div>
 </template>
 
 <script>
 
 import FilmList from "@/subcomponents/FilmList.vue"
+import SerieList from "@/subcomponents/SerieList.vue"
 
 export default {
   name: "MainComponent",
   props: {
-    movies: Array
+    movies: Array,
+    series: Array,
   },
   components: {
-    FilmList
+    FilmList,
+    SerieList,
   }
 }
 </script>

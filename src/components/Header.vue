71c91449f8376c8801ec2950e57e8d0391bdd93d
riccardoferro <riccardo.ferron8@gmail.com>
2022-04-21 12:38:41 +0200

    insert the input text and the button in the header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,27 @@
 <template>
-  
+    <header>
+        <div class="searchBar">
+            <!-- inserting here the user text -->
+            <input type="text" placeholder="search" v-model="TextToSearch">
+            <!-- sent the user text to the components APP -->
+            <button @click="$emit('text',TextToSearch)"> Search </button>
+        </div>
+    </header>
 </template>
 
 <script>
 export default {
 
+  name:"HeaderComponent",
+  
+  // variables
+  data(){
+      return{
+        TextToSearch: ""
+      };
+  }
 }
+
 </script>
 
 <style>
